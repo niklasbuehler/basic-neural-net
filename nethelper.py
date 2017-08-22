75c8dab91d638a4609debef41e7e8a3731c380ea
niklasbuehler <31211794+niklasbuehler@users.noreply.github.com>
2017-08-22 22:20:54 +0200

    Add files via upload

--- nethelper.py
+++ nethelper.py
@@ -5,96 +5,35 @@ import matplotlib.image
 from random import randint
 import scipy.misc
 
+# log starting information
+print "\033[1m\033[95m"
+print "[NEURAL NETWORK]\033[0m"
+print "\033[95m>Welcome to this basic approach of a neural network in python.\n>Type 'guide()' for help.\n>Type 'quit()' to quit.\n>Have fun! :)"
+print "\033[0m"
+
+# turn on interactive mode for pyplot
 matplotlib.pyplot.ion()
 
-def testrandom():
-    if 'net' not in globals():
-        error()
-        return
-    # load the MNIST test data CSV file into a list
-    test_data_file = open("mnist_dataset/mnist_test.csv", 'r')
-    test_data_list = test_data_file.readlines()
-    test_data_file.close()
-
-    # get random id
-    id = randint(0, len(test_data_list)-1)
-
+# creates a net based on the parameters
+def createnet(input_nodes, hidden_nodes, output_nodes, learning_rate, learning_epochs):
+    global net
+    net = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate, learning_epochs)
     # log info
-    print "\033[94m\033[1m"
-    print "[RANDOM TEST]\033[0m"
-    print "\033[94m  -Testing example #"+str(id)+"\033[0m"
-
-    # get the first test record
-    all_values = test_data_list[id].split(',')
-    # show the image in new window
-    __showimage(numpy.asfarray(all_values[1:]))
-    # get the guess from the network
-    netanswer = numpy.argmax(net.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01))
-    # print the label
-    print "\033[93m  >Guess: "+str(netanswer)+"\033[0m"
-    if netanswer == int(all_values[0]):
-        print "\033[92m>Correct"
-    else:
-        print "\033[91m>Wrong"
-    print "\033[0m"
-    pass
-
-def test(id):
-    if 'net' not in globals():
-        error()
-        return
-    # load the MNIST test data CSV file into a list
-    test_data_file = open("mnist_dataset/mnist_test.csv", 'r')
-    test_data_list = test_data_file.readlines()
-    test_data_file.close()
-
-    # log info
-    print "\033[94m\033[1m"
-    print "[TESTING EXAMPLE]\033[0m"
-    print "\033[94m  -Testing example #"+str(id)+"\033[0m"
-
-    # get the first test record
-    all_values = test_data_list[id].split(',')
-    # show the image in new window
-    __showimage(numpy.asfarray(all_values[1:]))
-    # get the guess from the network
-    netanswer = numpy.argmax(net.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01))
-    # print the label
-    print "\033[93m  >Guess: "+str(netanswer)+"\033[0m"
-    if netanswer == int(all_values[0]):
-        print "\033[92m>Correct"
-    else:
-        print "\033[91m>Wrong"
-    print "\033[0m"
-    pass
-
-def testimage(image_file_name):
-    if 'net' not in globals():
-        error()
-        return
-    # get the image and reshape it as array
-    img_array = scipy.misc.imread("images/"+image_file_name, flatten=True)
-    img_data = 255.0 - img_array.reshape(784)
-    img_data  =(img_data / 255.0 * 0.99) +0.01
-    # show the image
-    __showimage(img_data)
-
-    # log info
-    print "\033[94m\033[1m"
-    print "[TEST IMAGE]\033[0m"
-    print "\033[94m  -Grabbing image from 'images/"+image_file_name+"'\033[0m"
-
-    # get the guess from the network
-    netanswer = numpy.argmax(net.query(img_data))
-    # print the label
-    print "\033[93m>Guess: "+str(netanswer)
+    print "\033[1m\033[94m"
+    print "[INITIALIZE NETWORK]\033[0m"
+    print "\033[94m   stats:"
+    print "  -"+str(input_nodes)+" input nodes"
+    print "  -"+str(hidden_nodes)+" hidden nodes"
+    print "  -"+str(output_nodes)+" output nodes"
+    print "  -"+str(learning_rate)+" learning rate"
+    print "  -"+str(learning_epochs)+" learning epochs."
     print "\033[0m"
     pass
 
 # starts the training process
 def trainnet():
     if 'net' not in globals():
-        error()
+        __error()
         return
     # load MNIST training data CSV file into a list
     training_data_file = open("mnist_dataset/mnist_train.csv", 'r')
@@ -129,9 +68,10 @@ def trainnet():
     print "\033[0m"
     pass
 
+# tests the net with all testing data
 def testnet():
     if 'net' not in globals():
-        error()
+        __error()
         return
     # scorecard for how well the network performs, initially empty
     scorecard = []
@@ -176,17 +116,96 @@ def testnet():
     print "\033[0m"
     pass
 
-def __showimage(all_values):
-    # reshape the received data to an image array
-    image_array = all_values.reshape((28, 28))
-    # show this data in an extra window
-    matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
-    matplotlib.pyplot.show()
+# tests a specific example
+def test(id):
+    if 'net' not in globals():
+        __error()
+        return
+    # load the MNIST test data CSV file into a list
+    test_data_file = open("mnist_dataset/mnist_test.csv", 'r')
+    test_data_list = test_data_file.readlines()
+    test_data_file.close()
+
+    # log info
+    print "\033[94m\033[1m"
+    print "[TESTING EXAMPLE]\033[0m"
+    print "\033[94m  -Testing example #"+str(id)+"\033[0m"
+
+    # get the first test record
+    all_values = test_data_list[id].split(',')
+    # show the image in new window
+    __showimage(numpy.asfarray(all_values[1:]))
+    # get the guess from the network
+    netanswer = numpy.argmax(net.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01))
+    # print the label
+    print "\033[93m  >Guess: "+str(netanswer)+"\033[0m"
+    if netanswer == int(all_values[0]):
+        print "\033[92m>Correct"
+    else:
+        print "\033[91m>Wrong"
+    print "\033[0m"
+    pass
+
+# tests a random example
+def testrandom():
+    if 'net' not in globals():
+        __error()
+        return
+    # load the MNIST test data CSV file into a list
+    test_data_file = open("mnist_dataset/mnist_test.csv", 'r')
+    test_data_list = test_data_file.readlines()
+    test_data_file.close()
+
+    # get random id
+    id = randint(0, len(test_data_list)-1)
+
+    # log info
+    print "\033[94m\033[1m"
+    print "[RANDOM TEST]\033[0m"
+    print "\033[94m  -Testing example #"+str(id)+"\033[0m"
+
+    # get the first test record
+    all_values = test_data_list[id].split(',')
+    # show the image in new window
+    __showimage(numpy.asfarray(all_values[1:]))
+    # get the guess from the network
+    netanswer = numpy.argmax(net.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01))
+    # print the label
+    print "\033[93m  >Guess: "+str(netanswer)+"\033[0m"
+    if netanswer == int(all_values[0]):
+        print "\033[92m>Correct"
+    else:
+        print "\033[91m>Wrong"
+    print "\033[0m"
+    pass
+
+# tests an image in the '/images' folder
+def testimage(image_file_name):
+    if 'net' not in globals():
+        __error()
+        return
+    # get the image and reshape it as array
+    img_array = scipy.misc.imread("images/"+image_file_name, flatten=True)
+    img_data = 255.0 - img_array.reshape(784)
+    img_data  =(img_data / 255.0 * 0.99) +0.01
+    # show the image
+    __showimage(img_data)
+
+    # log info
+    print "\033[94m\033[1m"
+    print "[TEST IMAGE]\033[0m"
+    print "\033[94m  -Grabbing image from 'images/"+image_file_name+"'\033[0m"
+
+    # get the guess from the network
+    netanswer = numpy.argmax(net.query(img_data))
+    # print the label
+    print "\033[93m>Guess: "+str(netanswer)
+    print "\033[0m"
     pass
 
 def showidea(label):
     if 'net' not in globals():
-        error()
+        __error()
         return
 
     # log info
@@ -204,28 +223,13 @@ def showidea(label):
 
     pass
 
-def createnet(input_nodes, hidden_nodes, output_nodes, learning_rate, learning_epochs):
-    global net
-    net = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate, learning_epochs)
-    # log info
-    print "\033[1m\033[94m"
-    print "[INITIALIZE NETWORK]\033[0m"
-    print "\033[94m   stats:"
-    print "  -"+str(input_nodes)+" input nodes"
-    print "  -"+str(hidden_nodes)+" hidden nodes"
-    print "  -"+str(output_nodes)+" output nodes"
-    print "  -"+str(learning_rate)+" learning rate"
-    print "  -"+str(learning_epochs)+" learning epochs."
-    print "\033[0m"
-
-    pass
-
 def guide():
     print "\033[94m\033[1m"
     print "[GUIDE]\033[0m"
     print "\033[94m  -createnet(input_nodes, hidden_nodes, output_nodes, learning_rate, learning_epochs)"
     print "  -trainnet()"
     print "  -testnet()"
+    print "  -test(id)"
     print "  -testrandom()"
     print "  -testimage(image_file_name)"
     print "  -showidea(label)"
@@ -234,15 +238,17 @@ def guide():
 
     pass
 
-def error():
+def __error():
     print "\033[1m\033[91m"
     print "[ERROR]\033[0m"
     print "\033[91m  -You have to create a network first.\n  -Type guide() for help."
     print "\033[0m"
     pass
 
-# log starting information
-print "\033[1m\033[95m"
-print "[NEURAL NETWORK]\033[0m"
-print "\033[95m>Welcome to this basic approach of a neural network in python.\n>Type 'guide()' for help.\n>Type 'quit()' to quit.\n>Have fun! :)"
-print "\033[0m"
+def __showimage(all_values):
+    # reshape the received data to an image array
+    image_array = all_values.reshape((28, 28))
+    # show this data in an extra window
+    matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
+    matplotlib.pyplot.show()
+    pass
